5f4dea97c5b44823347c0e6f0a8d2e0b96b207d4
Lev Kadatscky <Lev.Kadaskyi@infiz.khpi.edu.ua>
2023-05-01 15:01:47 +0300

    fixed if-statements

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -9,9 +9,45 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def update_brie(self, item):
+        if item.quality < 50:
+            item.quality = item.quality + 1
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0 and item.quality < 50:
+            item.quality = item.quality + 1
+
+    def update_pass(self, item):
+        if item.quality < 50:
+            item.quality = item.quality + 1
+        if item.sell_in < 11 and item.quality < 50:
+            item.quality = item.quality + 1
+        if item.sell_in < 6 and item.quality < 50:
+            item.quality = item.quality + 1
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            item.quality = item.quality - item.quality
+
+    def update_sulfuras(self, item):
+        pass
+
+    def update_default(self, item):
+        if item.quality > 0:
+            item.quality = item.quality - 1
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0 < item.quality:
+            item.quality = item.quality - 1
+
     def update_quality(self):
         for item in self.items:
-            self.update_item_quality(item)
+            if item.name == BRIE:
+                self.update_brie(item)
+            elif item.name == BACKSTAGE:
+                self.update_pass(item)
+            elif item.name == SULFURAS:
+                self.update_sulfuras(item)
+            else:
+                self.update_default(item)
+            #self.update_item_quality(item)
 
     def update_item_quality(self, item):
         condition = item.name != BRIE and item.name != BACKSTAGE and item.name != SULFURAS
