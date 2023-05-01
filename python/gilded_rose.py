7961bd9789b03590538dc80b91f496895d864887
Lev Kadatscky <Lev.Kadaskyi@infiz.khpi.edu.ua>
2023-05-01 15:20:42 +0300

    fixed if-statements, added constants, added new functions

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
+SELL_IN_0 = 0
+ADJUSTMENT_MINUS = -1
+ADJUSTMENT_1 = 1
+SELL_IN_6 = 6
+SELL_IN_11 = 11
+QUALITY_0 = 0
+QUALITY_50 = 50
 SULFURAS = "Sulfuras, Hand of Ragnaros"
 BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert"
 BRIE = "Aged Brie"
@@ -9,69 +16,52 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
-    def update_brie(self, item):
-        if item.quality < 50:
-            item.quality = item.quality + 1
+    def adjust_sell_in(self, item):
         item.sell_in = item.sell_in - 1
-        if item.sell_in < 0 and item.quality < 50:
-            item.quality = item.quality + 1
+
+    def adjust_quality(self, adjustment, item):
+        new_quality = item.quality + adjustment
+        if QUALITY_50 >= new_quality >= QUALITY_0:
+            item.quality = new_quality
+
+    def update_brie(self, item):
+        self.adjust_quality(ADJUSTMENT_1, item)
+        self.adjust_sell_in(item)
+        if item.sell_in < SELL_IN_0:
+            self.adjust_quality(ADJUSTMENT_1, item)
 
     def update_pass(self, item):
-        if item.quality < 50:
-            item.quality = item.quality + 1
-        if item.sell_in < 11 and item.quality < 50:
-            item.quality = item.quality + 1
-        if item.sell_in < 6 and item.quality < 50:
-            item.quality = item.quality + 1
-        item.sell_in = item.sell_in - 1
-        if item.sell_in < 0:
+        self.adjust_quality(ADJUSTMENT_1, item)
+        if item.sell_in < SELL_IN_11:
+            self.adjust_quality(ADJUSTMENT_1, item)
+        if item.sell_in < SELL_IN_6:
+            self.adjust_quality(ADJUSTMENT_1, item)
+        self.adjust_sell_in(item)
+        if item.sell_in < SELL_IN_0:
             item.quality = item.quality - item.quality
 
     def update_sulfuras(self, item):
         pass
 
     def update_default(self, item):
-        if item.quality > 0:
-            item.quality = item.quality - 1
-        item.sell_in = item.sell_in - 1
-        if item.sell_in < 0 < item.quality:
-            item.quality = item.quality - 1
+        self.adjust_quality(ADJUSTMENT_MINUS, item)
+        self.adjust_sell_in(item)
+        if item.sell_in < SELL_IN_0:
+            self.adjust_quality(ADJUSTMENT_MINUS, item)
 
     def update_quality(self):
         for item in self.items:
-            if item.name == BRIE:
-                self.update_brie(item)
-            elif item.name == BACKSTAGE:
-                self.update_pass(item)
-            elif item.name == SULFURAS:
-                self.update_sulfuras(item)
-            else:
-                self.update_default(item)
-            #self.update_item_quality(item)
+            self.update_item_quality(item)
 
     def update_item_quality(self, item):
-        condition = item.name != BRIE and item.name != BACKSTAGE and item.name != SULFURAS
-        if condition:
-            self.adjust_quality(-1, item)
+        if item.name == BRIE:
+            self.update_brie(item)
+        elif item.name == BACKSTAGE:
+            self.update_pass(item)
+        elif item.name == SULFURAS:
+            self.update_sulfuras(item)
         else:
-            self.adjust_quality(1, item)
-        if item.name == BACKSTAGE:
-            if item.sell_in < 11 or item.sell_in < 6:
-                self.adjust_quality(1, item)
-        if item.name != SULFURAS:
-            item.sell_in = item.sell_in - 1
-        if item.sell_in < 0:
-            if condition:
-                self.adjust_quality(-1, item)
-            if item.name == BRIE:
-                self.adjust_quality(1, item)
-            elif item.name == BACKSTAGE:
-                item.quality = item.quality - item.quality
-
-    def adjust_quality(self, adjustment, item):
-        new_quality = item.quality + adjustment
-        if 50 >= new_quality >= 0:
-            item.quality = new_quality
+            self.update_default(item)
 
 
 class Item:
